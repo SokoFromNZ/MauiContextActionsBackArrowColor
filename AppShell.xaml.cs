7c05bbbda166735da0a24e953f381987d7c9bdc8
SokoFromNZ <soko17@soko.cc>
2022-12-05 11:13:28 +0100

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiContextActionsBackArrowColor;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiContextActionsBackArrowColor;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+
+    public ObservableCollection<string> Items { get; } = new() { "first", "second" };
+}
+
